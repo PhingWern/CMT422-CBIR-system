e5aeeecb93179e486b5bd8df9ea88861f7723e1b
HuiQin <huiqin@student.usm.my>
2021-06-19 15:06:24 +0800

    add feature extraction class

---
+++ src/utils/featureExtraction.java
@@ -0,0 +1,63 @@
+package utils;
+
+import net.semanticmetadata.lire.builders.GlobalDocumentBuilder;
+import net.semanticmetadata.lire.imageanalysis.features.global.*;
+import net.semanticmetadata.lire.utils.FileUtils;
+import net.semanticmetadata.lire.utils.LuceneUtils;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexWriter;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class featureExtraction {
+
+    public static void main(String[] args) throws Exception {
+//        Checking if the directory is empty
+        boolean isAccepted = false;
+        if(args.length>0){
+            File f = new File(args[0]);
+            if(f.exists() && f.isDirectory())
+                isAccepted=true;
+        }
+
+        if(!isAccepted){
+            throw new Exception("No directory is given. Indexing cannot undergone");
+        }
+
+//        Get image from directory
+        ArrayList<String> images = FileUtils.readFileLines(new File(args[0]),true);
+
+//        create document and store all files
+        GlobalDocumentBuilder globalDocumentBuilder = new GlobalDocumentBuilder(CEDD.class);
+//        feature need to extract
+        globalDocumentBuilder.addExtractor(ACCID.class);
+        globalDocumentBuilder.addExtractor(PHOG.class);
+        globalDocumentBuilder.addExtractor(Tamura.class);
+        globalDocumentBuilder.addExtractor(OpponentHistogram.class);
+
+//        Create Lucene index writer
+        IndexWriter writer = LuceneUtils.createIndexWriter("index",true,LuceneUtils.AnalyzerType.WhitespaceAnalyzer);
+
+        for (Iterator<String> it = images.iterator(); it.hasNext(); ){
+            String imageFilePath = it.next();
+            try{
+                BufferedImage bufferedImage = ImageIO.read(new FileInputStream(imageFilePath));
+                Document document = globalDocumentBuilder.createDocument(bufferedImage,imageFilePath);
+                writer.addDocument(document);
+            }catch (Exception e){
+                throw new Exception(e);
+            }
+
+        }
+
+//        Close writer
+        LuceneUtils.closeWriter(writer);
+
+    }
+}

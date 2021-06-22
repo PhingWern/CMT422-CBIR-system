07376286e4a989e570c26119ef3fa074edb6f3ad
PhingWern <phingwern0302@student.usm.my>
2021-06-23 00:15:47 +0800

    add comment

--- src/utils/FeatureExtraction.java
+++ src/utils/FeatureExtraction.java
@@ -19,17 +19,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+// implements Runnable to enable the class to run in thread
 public class FeatureExtraction implements Runnable{
 
+    // declare the path
     private String path;
 
+    // constructor of the class, set the directory path
     public FeatureExtraction(String path){
         this.path = path;
     }
 
     @Override
     public void run() {
-        //        Checking if the directory is empty
+        // Checking if the directory is empty
         boolean isAccepted = false;
         if(path.length()>0){
             File f = new File(path);
@@ -46,51 +49,63 @@ public class FeatureExtraction implements Runnable{
         }
 
         try{
-            //        Get image from directory
+            // Get image from directory
             ArrayList<String> images = FileUtils.readFileLines(new File(path),true);
 
-            //        create document and store all files
+            // create document and store all files
             GlobalDocumentBuilder globalDocumentBuilder = new GlobalDocumentBuilder(ACCID.class);
-            //        feature need to extract
+            // feature need to extract
             //globalDocumentBuilder.addExtractor(ACCID.class);
             globalDocumentBuilder.addExtractor(PHOG.class);
             globalDocumentBuilder.addExtractor(Tamura.class);
             globalDocumentBuilder.addExtractor(OpponentHistogram.class);
 
-            //        Create Lucene index writer
+            // Create Lucene index writer
             IndexWriter writer = LuceneUtils.createIndexWriter("index",true,LuceneUtils.AnalyzerType.WhitespaceAnalyzer);
 
+            // initialize the i index
             int i = 1;
+            // for each image in the directory
             for (Iterator<String> it = images.iterator(); it.hasNext(); ){
+                //
                 String imageFilePath = it.next();
+
                 try{
                     BufferedImage bufferedImage = ImageIO.read(new FileInputStream(imageFilePath));
                     Document document = globalDocumentBuilder.createDocument(bufferedImage,imageFilePath);
                     writer.addDocument(document);
+                    // set the progress of progress bar in index scene
                     IndexScene.indexProgress.setProgress(i / (double)images.size());
+                    // declare finalI
                     int finalI = i;
+                    // platfrom.runlater to update the GUI outside the thread
                     Platform.runLater(new Runnable() {
                         @Override
                         public void run() {
+                            // update the text in indexLabel
                             IndexScene.indexLabel.setText(" Indexing images ... ( " + finalI + " / " + images.size() + " )");
                         }
                     });
                 }catch (Exception e){
+                    // if any error occurs, print it to the console
                     throw new Exception(e);
                 }
                 i++;
             }
 
-            //        Close writer
+            // Close writer
             LuceneUtils.closeWriter(writer);
+            // platfrom.runlater to update the GUI outside the thread
             Platform.runLater(new Runnable() {
                 @Override
                 public void run() {
+                    // update the text in indexLabel
                     IndexScene.indexLabel.setText(" Finish Indexing! ");
                 }
             });
 
         } catch (Exception e) {
+            // if any error occurs, print it to the console
             e.printStackTrace();
         }
 
